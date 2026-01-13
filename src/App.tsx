c15d6d16cd023ef88c69cbbeaf60ecf7a1503f77
radzhiv <radzhivkrishna@gmail.com>
2026-01-13 12:08:12 +0530

    Initialized React App with Tailwind

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+
+  return (
+    <>
+    <div className="p-2 min-h-screen w-full flex flex-col items-center justify-start">
+      <h1 className="text-2xl font-semibold">Interactive Dashboard</h1>
+    </div>
+    </>
+  )
+}
+
+export default App
